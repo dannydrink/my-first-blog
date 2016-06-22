a0b2ee888e7e725a1c8cd6f6c01d847f0924031a
Daniel <daniel.drinkwater@cerner.com>
2016-06-22 15:38:03 -0500

    Changed the HTML for the site.

---
+++ Scripts/atm-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\dd033493\djangogirls\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'automium==0.2.6','console_scripts','atm'
+__requires__ = 'automium==0.2.6'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('automium==0.2.6', 'console_scripts', 'atm')()
+    )
